90571976c5dd7b10ace74414792a3a8f644f0eb2
ntejaa5-ui <ntejaa5@gmail.com>
2026-01-28 08:07:59 +0530

    Change default port from 5000 to 8080

--- main.py
+++ main.py
@@ -142,5 +142,5 @@ def list_conversations():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
